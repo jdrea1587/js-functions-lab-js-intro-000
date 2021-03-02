bc7dcf63d71d3fd062c9f3d9d158df728453446f
Joshua Rea <joshdanielrea@gmail.com>
2021-03-02 19:22:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,3 +22,16 @@ describe('happyHolidaysTo(name)', () => {
 function happyHolidaysTo(name){
   return (`Happy holidays, ${name}!`)
 }
+/*
+
+  describe('happyCustomHolidayTo(holiday, name)', () => {
+    it('returns "Happy ${holiday}, ${name}!"', () => {
+      expect(happyCustomHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+      expect(happyCustomHolidayTo('New Year', 'everyone')).toEqual('Happy New Year, everyone!')
+    })
+  })
+  */
+  function happyCustomHolidayTo(holiday, name){
+    return (`Happy ${holiday}, ${name}!`)
+  }
+  
