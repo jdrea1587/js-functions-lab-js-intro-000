e6f8cac0659a00dd00b024adacdea264286b5338
Joshua Rea <joshdanielrea@gmail.com>
2021-03-02 19:09:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code below!
+//write your code below!
+/*
+describe('functions', () => {
+  describe('happyHolidays', () => {
+    it('returns "Happy holidays!"', () => {
+      expect(happyHolidays()).toEqual('Happy holidays!')
+    })
+  })
+  */
+  happyHolidays(){
+    return ('happyHolidays')
+  }
+  
